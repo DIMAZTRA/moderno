0e971802ab20f1c4d17b2e462bc3c44220ee1403
DIMAZTRA <moisejchenkov98@mail.ru>
2019-09-19 18:15:12 +0300

    added product-filter slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function() {
-
-
+    var mixer = mixitup('.products__inner-box');
+    $(".ret-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 
 })
